12901c893b69f971b49beaf590e69b2f018e69c5
Adriana Bermudez <adriana.berm@gmail.com>
2019-02-13 21:16:06 -0500

    [ui-test] - initial layout structure

---
+++ dist/static/js/main.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImludGVyYWN0aW9ucy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiIl19
